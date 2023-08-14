7e48cc71aad66e6d9284def36a03f7caa398bd0b
Joo1josiah <eyosu21@gmail.com>
2023-08-14 03:40:41 -0700

    c dprogramming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,10 @@
  */
 int main(void) 
 {
+	/*
+	 * printing using write function
+	 */
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return 1;
+	return (1);
 }
