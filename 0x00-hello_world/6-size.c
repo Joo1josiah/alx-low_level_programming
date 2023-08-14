2f13944957aa4e527488f0e384f540776f86f4d0
Joo1josiah <eyosu21@gmail.com>
2023-08-14 12:57:13 -0700

    c programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,12 @@
 
 /**
  *  main - Entry point of the program
- *
  * Return: Always 0 (success)
  */
 int main(void)
 
 {
-	/*
-	 * printing sizes of various types
-	 */
+	/* printing sizes of various types */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
