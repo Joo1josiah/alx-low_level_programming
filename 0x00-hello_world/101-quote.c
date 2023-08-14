9960fc8f1d33eb04ba58c9bbc32626d4f171d8b9
Joo1josiah <eyosu21@gmail.com>
2023-08-14 04:26:59 -0700

    c dprogramming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
-/*
+
+/**
  * main - starts the program
+ *
  *Return:always 1 (failure)
  */
+
 int main(void) 
 {
+
 	/*
 	 * printing using write function
 	 */
+
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
