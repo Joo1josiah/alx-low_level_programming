c8a1db3bfa3e93199744f4b313344cc1d3dff00c
Joo1josiah <eyosu21@gmail.com>
2023-08-15 14:28:24 -0700

    c functions and loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,10 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+	int last_digit = n % 10;
 
-    if (last_digit < 0)
-        last_digit *= -1;
-
-    _putchar('0' + last_digit);
-    return last_digit;
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
