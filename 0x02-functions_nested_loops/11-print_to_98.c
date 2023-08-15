d8cca0e57ce9358dbff9e170affe5cd398c5c9e7
Joo1josiah <eyosu21@gmail.com>
2023-08-15 15:35:05 -0700

    c functions and loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,9 @@ void print_to_98(int n)
 {
     if (n <= 98)
     {
-        for (int i = n; i <= 98; i++)
+	    int i;
+
+        for (i = n; i <= 98; i++)
         {
             if (i != n)
             {
@@ -40,7 +42,9 @@ void print_to_98(int n)
     }
     else
     {
-        for (int i = n; i >= 98; i--)
+	    int i;
+
+        for (i = n; i >= 98; i--)
         {
             if (i != n)
             {
