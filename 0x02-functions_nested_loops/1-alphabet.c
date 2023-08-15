31f8ee5f39c41f5bc6da5c08b9dee6750e943250
Joo1josiah <eyosu21@gmail.com>
2023-08-15 07:41:05 -0700

    c functions and loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,8 @@
 
 /**
  * print alphabet - Prints the lowercase alphabet followed by a new line
- * Description: This function prints all lc letters
+ * Description: This function prints all the lowercase letters of the
+ * English alphabet in sequence, followed by a newline character.
  */
 void print_alphabet(void)
 {
