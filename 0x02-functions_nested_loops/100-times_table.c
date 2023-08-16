67987eecf1e7c15810888dc801f812c053dc105a
Joo1josiah <eyosu21@gmail.com>
2023-08-15 23:56:49 -0700

    c functions and loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,62 @@
 #include "main.h"
 
 /**
+ * print_formatted_number - Prints a number with proper formatting.
+ * @number: The number to be printed.
+ */
+void print_formatted_number(int number)
+{
+    if (number < 10)
+    {
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(number + '0');
+    }
+    else if (number < 100)
+    {
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(number / 10 + '0');
+        _putchar(number % 10 + '0');
+    }
+    else
+    {
+        _putchar(' ');
+        _putchar(number / 100 + '0');
+        _putchar((number / 10) % 10 + '0');
+        _putchar(number % 10 + '0');
+    }
+}
+
+/**
  * print_times_table - Prints the n times table.
  * @n: The number up to which the times table should be printed.
  */
 void print_times_table(int n)
 {
-	int i, j, result;
+    int i, j, result;
 
-	if (n < 0 || n > 15)
-		return;
+    if (n < 0 || n > 15)
+        return;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            result = i * j;
 
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
-			}
-			else if (result < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(result / 100 + '0');
-				_putchar((result / 10) % 10 + '0');
-				_putchar(result % 10 + '0');
-			}
+            if (j == 0)
+                _putchar('0');
+            else
+                print_formatted_number(result);
 
-			if (j != n)
-			{
-				_putchar(',');
-			}
-		}
-		_putchar('\n');
-	}
+            if (j != n)
+            {
+                _putchar(',');
+            }
+        }
+        _putchar('\n');
+    }
 }
+
