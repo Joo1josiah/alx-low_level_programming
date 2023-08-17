538b69da4347ae0c68d6abd23b8127459f952702
Joo1josiah <eyosu21@gmail.com>
2023-08-17 05:52:00 -0700

    debugging is fun

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,23 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && a > c)
 	{
-		if (b > c)
-		{
-			largest = a;
-		}
-		else if (c > a)
-		{
-			largest = c;
-		}
+		largest = a;
 	}
-	else if (b > c)
+	else if (b > a && b > c)
 	{
-	largest = b;
+		largest = b;
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
-}	
+
+	return (largest);
+}
