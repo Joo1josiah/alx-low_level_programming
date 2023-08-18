508963717260b6f05379a5e33c1ab3d248730cf2
Joo1josiah <eyosu21@gmail.com>
2023-08-18 00:50:18 -0700

    more functions and loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,14 @@
 #include "main.h"
+/**
+ * _putchar - writes a character to the standard output
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
 
 /**
  * print_numbers - prints the numbers from 0 to 9 followed by a new line.
