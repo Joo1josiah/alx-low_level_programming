03a47886ee31728dce607cd0fb8b2c324bb21bcf
Joo1josiah <eyosu21@gmail.com>
2023-08-21 07:14:01 -0700

    memory and pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string, followed by a new line.
+ * @str: Pointer to the string to be processed.
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	start = (length - 1) / 2 + 1;
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+
+	_putchar('\n');
+}
