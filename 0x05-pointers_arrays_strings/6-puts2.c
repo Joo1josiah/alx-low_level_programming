262e4cc902f5716f12c967c3c8e2cc81174414cf
Joo1josiah <eyosu21@gmail.com>
2023-08-22 06:59:42 -0700

    memory and pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len =0;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
+		len++;
+
+	for (; i <= len; i += 2)
 	{
-		printf("%c", str[i]);
-		i += 2;
+		printf("%c", str[i])
 	}
 	printf("\n");
 }
